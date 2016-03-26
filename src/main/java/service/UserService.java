5c238375bcd6a1535ef6bfa81f64b130ea947adb
wanghy <why3046380@163.com>
2016-03-26 20:41:07 +0800

    ff

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,5 +1,6 @@
 package service;
 
+import org.hibernate.bytecode.spi.NotInstrumentedException;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.test.annotation.Rollback;
@@ -11,7 +12,7 @@ import javax.xml.ws.ServiceMode;
 /**
  * Created by wanghy on 2016/3/26.
  */
-@Transactional(propagation = Propagation.REQUIRED,rollbackFor = {Exception.class})
+@Transactional(propagation = Propagation.REQUIRED,rollbackFor = {Exception.class, NotInstrumentedException.class})
 public interface UserService {
     public  void  Register(String username,String password) throws Exception;
 }
