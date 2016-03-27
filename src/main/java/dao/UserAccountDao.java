aa5dcfa090dff7cd858ff61a12f0eb8b4b706b7f
wanghy <why3046380@163.com>
2016-03-27 21:18:13 +0800

    ff

--- src/main/java/dao/UserAccountDao.java
+++ src/main/java/dao/UserAccountDao.java
@@ -3,11 +3,15 @@ package dao;
 import domain.UserAccount;
 import viewmodel.UserInfoViewModel;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by wanghy on 2016/3/26.
  */
 public interface UserAccountDao {
-    void  Register(UserAccount userAccount);
+    void  Register(@NotNull(message = "请输入正确的用户信息") @Valid UserAccount userAccount);
     UserAccount GetModel(String telphone);
-    UserInfoViewModel GetModel(int id);
+    UserInfoViewModel GetModel(@Min(value = 1,message = "请输入正确的用户编号") int id);
 }
